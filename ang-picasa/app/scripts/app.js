26490bd2db4f2265580afe27a0ef2d4a7db4697f
Ido Green <greenido@gmail.com>
2013-04-30 22:07:49 +0300

    Add signIn with g+ button

--- ang-picasa/app/scripts/app.js
+++ ang-picasa/app/scripts/app.js
@@ -1,4 +1,16 @@
 'use strict';
 
-var app = angular.module('testApp', ['angularPicasa']);
+var app = angular.module('testApp', ['angularPicasa','directive.g+signin']);
 
+//
+// Callback for Google+ Sign-In
+//
+function signinCallback(authResult) {
+  console.log( authResult);
+  if (authResult['access_token']) {
+    console.log("cool! the user successfully authorized our demo app");
+  } else if (authResult['error']) {
+    // User has not authorized our amazing app - No comments for you!
+    console.log("error");
+  }
+} 
